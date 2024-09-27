1906edd3f2649060a899232004da3941a50d2a06
Pedro Calleja <pecalleja@gmail.com>
2024-09-27 15:51:50 -0400

    do not use one line version

--- src/arrays/contains_duplicate.py
+++ src/arrays/contains_duplicate.py
@@ -3,4 +3,9 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
