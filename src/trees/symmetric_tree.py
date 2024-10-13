bff631552dde57424c1ed71b10747d2c443b2732
Pedro Calleja <pecalleja@gmail.com>
2024-10-13 14:00:01 -0400

    101. Symmetric Tree
    https://leetcode.com/problems/symmetric-tree/description/

---
+++ src/trees/symmetric_tree.py
@@ -0,0 +1,20 @@
+from src.trees import TreeNode
+
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+
+        def is_mirror(left, right):
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+            if left.val != right.val:
+                return False
+            return is_mirror(left.left, right.right) and is_mirror(
+                left.right, right.left
+            )
+
+        return is_mirror(root.left, root.right)
