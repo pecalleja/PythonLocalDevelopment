ff057870048e8fc0f4689903ec64f4ca5528025c
Pedro Calleja <pecalleja@gmail.com>
2024-10-17 11:04:40 -0400

    112. Path Sum
    https://leetcode.com/problems/path-sum/description/

---
+++ src/trees/leaf_count.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from src.trees import TreeNode
+
+
+class Solution:
+    def countLeaves(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return 1
+        return self.countLeaves(root.left) + self.countLeaves(root.right)
---
+++ src/trees/path_sum.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+from src.trees import TreeNode
+
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def dfs(node, currSum):
+            if not node:
+                return False
+            currSum += node.val
+            if not node.left and not node.right:
+                return currSum == targetSum
+            return dfs(node.left, currSum) or dfs(node.right, currSum)
+
+        return dfs(root, 0)
