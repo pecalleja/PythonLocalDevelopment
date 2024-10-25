f58f8264baeaa6dddb07adcb2c80ced5a4dc6e26
Pedro Calleja <pecalleja@gmail.com>
2024-10-25 07:35:32 -0400

    144. Binary Tree Preorder Traversal
    https://leetcode.com/problems/binary-tree-preorder-traversal/description/

---
+++ tests/trees/preorder_traversal_test.py
@@ -0,0 +1,19 @@
+import pytest
+
+from src.trees.preorder_traversal import Solution
+
+
+@pytest.mark.parametrize(
+    "values, expected",
+    [
+        ([1, None, 2, 3], [1, 2, 3]),
+        ([], []),
+        ([1], [1]),
+        ([1, 2, 3], [1, 2, 3]),
+        ([1, None, 2, None, 3], [1, 2, 3]),
+    ],
+)
+def test_preorderTraversal(build_tree_from_list, values, expected):
+    sol = Solution()
+    root = build_tree_from_list(values)
+    assert sol.preorderTraversal(root) == expected
