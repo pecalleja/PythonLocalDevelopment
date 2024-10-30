473eca5648ccfc19998127ec7a318403b987cb83
Pedro Calleja <pecalleja@gmail.com>
2024-10-30 08:35:26 -0400

    206. Reverse Linked List
    https://leetcode.com/problems/reverse-linked-list/description/

--- src/linkedlists/reverse_linked_list.py
+++ src/linkedlists/reverse_linked_list.py
@@ -5,10 +5,11 @@ from src.linkedlists import ListNode
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if (not head) or (not head.next):
-            return head
-
-        p = self.reverseList(head.next)
-        head.next.next = head
-        head.next = None
-        return p
+        prev_node = None
+        curr_node = head
+        while curr_node:
+            next_node = curr_node.next
+            curr_node.next = prev_node
+            prev_node = curr_node
+            curr_node = next_node
+        return prev_node
