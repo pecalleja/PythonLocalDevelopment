9186fa62b7e7c41b76fab495358a1d2a6fd9b80c
Pedro Calleja <pecalleja@gmail.com>
2024-11-02 17:32:50 -0400

    167. Two Sum II - Input Array Is Sorted
    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/

---
+++ tests/twopointers/two_sum_ii_test.py
@@ -0,0 +1,20 @@
+import pytest
+
+from src.twopointers.two_sum_ii import Solution
+
+
+@pytest.mark.parametrize(
+    "numbers, target, expected",
+    [
+        ([2, 7, 11, 15], 9, [1, 2]),
+        ([2, 3, 4], 6, [1, 3]),
+        ([-1, 0], -1, [1, 2]),
+        ([1, 2, 3, 4, 4], 8, [4, 5]),
+        ([1, 3, 4, 5, 7, 10, 11], 9, [3, 4]),
+        ([1, 2, 3, 4, 5], 10, []),
+        ([1, 5, 6, 8, 11], 12, [1, 5]),
+    ],
+)
+def test_twoSum(numbers, target, expected):
+    sol = Solution()
+    assert sol.twoSum(numbers, target) == expected
