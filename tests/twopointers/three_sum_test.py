7ea92380080b0a8c6aca55d2b3b9e7b00531d412
Pedro Calleja <pecalleja@gmail.com>
2024-11-03 18:47:06 -0500

    15. 3Sum
    https://leetcode.com/problems/3sum/description/

---
+++ tests/twopointers/three_sum_test.py
@@ -0,0 +1,25 @@
+import pytest
+
+from src.twopointers.three_sum import Solution
+
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
+        ([0, 1, 1], []),
+        ([0, 0, 0, 0], [[0, 0, 0]]),
+        ([-2, 0, 1, 1, 2], [[-2, 0, 2], [-2, 1, 1]]),
+        ([], []),
+        ([0], []),
+        ([1, 2, -2, -1], []),
+    ],
+)
+def test_threeSum(nums, expected):
+    sol = Solution()
+    result = sol.threeSum(nums)
+
+    def sort_triplets(triplets):
+        return sorted([sorted(triplet) for triplet in triplets])
+
+    assert sort_triplets(result) == sort_triplets(expected)
