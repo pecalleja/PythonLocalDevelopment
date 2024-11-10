c7ee3a8b773a4e22cd7baed251b0a15660a07f37
Pedro Calleja <pecalleja@gmail.com>
2024-11-09 19:33:04 -0500

    704. Binary Search
    https://leetcode.com/problems/binary-search/description/

---
+++ tests/binarysearch/binary_search_test.py
@@ -0,0 +1,23 @@
+import pytest
+
+from src.binarysearch.binary_search import Solution
+
+
+@pytest.mark.parametrize(
+    "nums, target, expected",
+    [
+        ([1, 2, 3, 4, 5, 6], 4, 3),  # Target in the middle of the list
+        ([1, 2, 3, 4, 5, 6], 1, 0),  # Target at the start
+        ([1, 2, 3, 4, 5, 6], 6, 5),  # Target at the end
+        ([1, 3, 5, 7, 9, 11], 2, -1),  # Target not in the list
+        ([], 5, -1),  # Empty list
+        ([5], 5, 0),  # Single element list, target found
+        ([5], 4, -1),  # Single element list, target not found
+        ([1, 3, 5, 7, 9], 7, 3),  # Odd length list
+        ([-10, -3, 0, 5, 9], 0, 2),  # List with negative and positive numbers
+        ([-10, -3, 0, 5, 9], -3, 1),  # Target is a negative number
+    ],
+)
+def test_search(nums, target, expected):
+    sol = Solution()
+    assert sol.search(nums, target) == expected
