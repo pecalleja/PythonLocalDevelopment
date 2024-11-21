cd609960191d182f704ce5aae256d61eb89e303c
Pedro Calleja <pecalleja@gmail.com>
2024-11-21 10:19:38 -0500

    143. Reorder List
    https://leetcode.com/problems/reorder-list/description/

---
+++ src/linkedlists/reorder_list.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+from src.linkedlists import ListNode
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next or not head.next.next:
+            return
+
+        # Step 1: Find the middle of the list
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Step 2: Reverse the second half of the list
+        prev, curr = None, slow.next
+        slow.next = None
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        # Step 3: Merge the two halves
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
