4ca5aa7b83702987d1c0fc9498d117c0a7220dc7
Pedro Calleja <pecalleja@gmail.com>
2024-11-27 09:10:23 -0500

    23. Merge k Sorted Lists
    https://leetcode.com/problems/merge-k-sorted-lists/description/

--- src/linkedlists/__init__.py
+++ src/linkedlists/__init__.py
@@ -2,3 +2,7 @@ class ListNode:
     def __init__(self, val=0, next_node=None):
         self.val = val
         self.next = next_node
+
+    def __lt__(self, other):
+        # This ensures that ListNode can be compared in the heap
+        return self.val < other.val
---
+++ src/linkedlists/merge_k_sorted_lists.py
@@ -0,0 +1,32 @@
+from heapq import heappop
+from heapq import heappush
+
+from src.linkedlists import ListNode
+
+
+class Solution:
+    def mergeKLists(self, lists):
+        if not lists:
+            return None
+
+        heap = []
+        # Push the head node of each list into the heap
+        for i, node in enumerate(lists):
+            if node:
+                heappush(heap, node)
+
+        # Dummy head for the result list
+        dummy = ListNode(0)
+        current = dummy
+
+        while heap:
+            # Pop the smallest node from the heap
+            smallest = heappop(heap)
+            current.next = smallest
+            current = current.next
+            # If the list of the smallest node has more elements,
+            # push the next node to the heap
+            if smallest.next:
+                heappush(heap, smallest.next)
+
+        return dummy.next
