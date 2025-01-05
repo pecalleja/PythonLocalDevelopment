69abccc9aec35d20119ba75a256d9bb37cb9bc0f
Pedro Calleja <pecalleja@gmail.com>
2025-01-05 18:58:37 -0500

    code refactoring

--- src/neetcode/arrays/climbing_stairs.py
+++ src/neetcode/arrays/climbing_stairs.py
@@ -1,12 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 0 or n == 1:
+        if n <= 1:
             return 1
 
         first, second = 1, 1
         for _ in range(2, n + 1):
-            third = first + second
-            first = second
-            second = third
+            first, second = second, first + second
 
         return second
