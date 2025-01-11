12f6437ec96a7628bc28f50489c12eb1cbb67aa2
Pedro Calleja <pecalleja@gmail.com>
2025-01-11 12:45:08 -0500

    code refactor

--- src/neetcode/arrays/fibonacci.py
+++ src/neetcode/arrays/fibonacci.py
@@ -1,17 +1,10 @@
 class Solution:
     def fib(self, n: int) -> int:
-        cache = {}
+        cache = {0: 0, 1: 1}
 
         def recursive_fib(e):
-            if e in cache:
-                return cache[e]
-
-            if e < 2:
-                return e
-            else:
-                ans = recursive_fib(e - 1) + recursive_fib(e - 2)
-
-            cache[e] = ans
-            return ans
+            if e not in cache:
+                cache[e] = recursive_fib(e - 1) + recursive_fib(e - 2)
+            return cache[e]
 
         return recursive_fib(n)
